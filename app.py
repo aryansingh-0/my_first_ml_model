824fc4801033cbd8aae0ffba6f79eb463b321961
ARYAN SINGH <151323356+aryansingh-0@users.noreply.github.com>
2025-07-14 12:04:36 +0530

    Update app.py

--- app.py
+++ app.py
@@ -5,17 +5,17 @@ import joblib
 model = joblib.load("model.pkl")
 
 # Streamlit UI
-st.title("📧 Email Spam Classifier")
+st.title("Email Spam Classifier")
 st.write("Enter an email or message below, and the model will predict whether it's spam or not.")
 
 # User input
-user_input = st.text_input("✍️ Your Message")
+user_input = st.text_input("Your Message")
 
 # Predict
 if st.button("🔍 Predict"):
     prediction = model.predict([user_input])[0]  # Get scalar value from array
 
     if prediction == 1:
-        st.error("🚫 This message is **SPAM**. Be cautious before clicking links or replying.")
+        st.error("This message is **SPAM**. Be cautious before clicking links or replying.")
     else:
-        st.success("✅ This message is **NOT SPAM**. It seems safe.")
+        st.success("This message is **NOT SPAM**. It seems safe.")
